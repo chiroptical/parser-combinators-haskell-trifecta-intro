135b91d731dea4e927d56a0f505cc082633a67e1
Barry Moore <moore0557@gmail.com>
2019-06-13 20:23:05 -0400

    Use shouldParse to create Expectation

--- code/rational/test/Spec.hs
+++ code/rational/test/Spec.hs
@@ -1,34 +1,44 @@
 import           Control.DeepSeq
 import           Control.Exception
 import           Data.Ratio        ((%))
+import           Debug.Trace
 import           Lib
 import           Test.Hspec
+import           Test.HUnit.Lang   (assertEqual, assertFailure)
 import           Text.Trifecta
-
--- Don't understand how to deal with Failure
--- from Trifecta correctly. Our parseToEither
--- function is a hack. The `Eq (Result a)`
--- instance is also incomplete.
-parseToEither :: Result a -> Either String a
-parseToEither (Success x) = Right x
-parseToEither (Failure e) = Left "Failure"
 
 instance Eq a => Eq (Result a) where
   Success x == Success y = x == y
+  _ == _ = error "Failures are uncomparable"
 
 instance NFData a => NFData (Result a) where
   rnf (Success x) = rnf x
+  rnf (Failure e) = ()
+
+shouldParse :: (Show a, Eq a) => a -> Result a -> Expectation
+shouldParse x (Success x') = assertEqual "Parse succeeds" x x'
+shouldParse _ (Failure _)  = assertFailure "Parse failure"
 
 main :: IO ()
 main =
   hspec $ do
     describe "parseRational" $ do
-      it "should return Success 1 % 1" $ do
-        p parseRational "1/1" `shouldBe` Success (1 % 1)
-      it "should emit 'Ratio has zero denominator' exception" $ do
+      it "should succeed on \"1/1\"" $ do
+        shouldParse example $ p parseRational "1/1"
+      it "should emit on invalid ratio \"1/0\"" $ do
         (evaluate . force) (p parseRational "1/0") `shouldThrow` anyException
+      it "should emit on partial ratio \"1/\"" $ do
+         shouldParse example (p parseRational "1/") `shouldThrow` anyException
+      it "should emit on partial ratio \"/1\"" $ do
+         shouldParse example (p parseRational "/1") `shouldThrow` anyException
     describe "parseRational'" $ do
-      it "should return Success 1 % 1" $ do
-        p parseRational' "1/1" `shouldBe` Success (1 % 1)
-      it "should return Left \"Failure\"" $ do
-        parseToEither (p parseRational' "1/0") `shouldBe` Left "Failure"
+      it "should succeed on \"1/1\"" $ do
+        shouldParse example $ p parseRational' "1/1"
+      it "should emit on invalid ratio \"1/0\"" $ do
+        shouldParse example (p parseRational' "1/0") `shouldThrow` anyException
+      it "should emit on partial ratio \"1/\"" $ do
+         shouldParse example (p parseRational' "1/") `shouldThrow` anyException
+      it "should emit on partial ratio \"/1\"" $ do
+         shouldParse example (p parseRational' "/1") `shouldThrow` anyException
+  where
+    example = 1 % 1
